05e6cffa04d5e6f9cb7ecadadeeb1643bc587bf2
NhatTaiC <nhattai0501@gmail.com>
2024-04-18 23:57:06 +0700

    Update Navbar & Footer Page

--- Project/src/src_laravel_10.48_change_ui/21-training-laravel/resources/views/dashboard.blade.php
+++ Project/src/src_laravel_10.48_change_ui/21-training-laravel/resources/views/dashboard.blade.php
@@ -6,25 +6,25 @@
 </head>
 <body>
 {{--Navbar--}}
-<nav class="navbar navbar-dark bg-dark navbar-expand-lg mb-5" style="background-color: #e3f2fd;">
+<nav class="navbar navbar-dark bg-dark navbar-expand-lg mb-5 text-white fixed-top" style="background-color: #e3f2fd;">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Laravel Training - Group H</a>
+        <a class="navbar-brand" href="#" style="font-weight: 700; color: white">Laravel Training - Group H</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav m-auto">
                 @guest
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">Đăng Nhập </a>
+                        <a class="nav-link" href="{{ route('login') }}" style="font-weight: 700; color: white">Đăng Nhập </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('user.createUser') }}">Đăng Ký</a>
+                        <a class="nav-link" href="{{ route('user.createUser') }}" style="font-weight: 700; color: white">Đăng Ký</a>
                     </li>
                 @else
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('signout') }}">Đăng Xuất</a>
+                        <a class="nav-link" href="{{ route('signout') }}" style="font-weight: 700; color: white">Đăng Xuất</a>
                     </li>
                 @endguest
             </ul>
@@ -35,7 +35,7 @@
 {{--Footer--}}
 <div class="container-fluid fixed-bottom bg-dark">
     <div class="container-fluid text-white py-2 my-2">
-        <p class="mx-auto my-auto text-center">Lập trình web &#169 01/2024 - Nhóm H</p>
+        <p class="mx-auto my-auto text-center" style="font-weight: 700; color: white">Lập trình web &#169 01/2024 - Nhóm H</p>
     </div>
 </div>
 @yield('content')
