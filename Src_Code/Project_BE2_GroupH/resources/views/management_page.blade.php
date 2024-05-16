46a2f6f155d9e5466a0e0dc0e455c30a9f89c079
MaiThanhTai204 <taithanhmai204@gmail.com>
2024-05-16 22:47:58 +0700

    taikhoan

--- Src_Code/Project_BE2_GroupH/resources/views/management_page.blade.php
+++ Src_Code/Project_BE2_GroupH/resources/views/management_page.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
     <link rel="stylesheet" href="{{ asset('css/management_page.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/btnsapxep.css') }}">
+  <style>
+    .btn-sapxep{
+        text-align: center;
+    }
+  </style>
 
     {{-- Section Title --}}
     <section id="section-title">
@@ -15,14 +21,61 @@
     {{-- Section Options --}}
     <section id="section-option">
         <div class="container-fluid mt-5 center">
-            <ul class="ul-option">
-                <li><a href="{{ route('user.list') }}" class="ul-item text-decoration-none">Management TaiKhoan</a></li>
-                <li><a href="{{ route('loaixe.list_loaixe') }}" class="ul-item text-decoration-none">Management LoaiXe</a></li>
-                <li><a href="{{ route('xe.list_xe') }}" class="ul-item text-decoration-none">Management Xe</a></li>
-                <li><a href="#" class="ul-item text-decoration-none">Management ChiTietXe</a></li>
-                <li><a href="#" class="ul-item text-decoration-none">Management ChiNhanh</a></li>
-                <li><a href="#" class="ul-item text-decoration-none">Management LichHen</a></li>
-                <li><a href="#" class="ul-item text-decoration-none">Management LienHe</a></li>
+            <div class="row">
+                <div class="col-md-12 btn-sapxep">
+
+                    <button class="btn">
+                    <a href="{{ route('user.list') }}" class="ul-item text-decoration-none">Management TaiKhoan</a>
+                </div>
+            </div>
+</button>
+<div class="row">
+    <div class="col-md-4">
+    <button class="btn">
+<a href="#" class="ul-item text-decoration-none">Management LienHe</a>
+</button>
+             
+    </div>
+    <div class="col-md-4">
+
+        <button class="btn">
+        <a href="#" class="ul-item text-decoration-none">Management LichHen</a>
+        </button>
+    </div>
+    <div class="col-md-4">
+
+        <button class="btn">
+        <a href="#" class="ul-item text-decoration-none">Management ChiNhanh</a>
+        </button>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-4">
+        <button class="btn">
+        <a href="#" class="ul-item text-decoration-none">Management ChiTietXe</a>
+        </button>
+
+    </div>
+    <div class="col-md-4">
+
+        <button class="btn">
+        <a href="{{ route('xe.list_xe') }}" class="ul-item text-decoration-none">Management Xe</a>
+        </button>
+    </div>
+    <div class="col-md-4">
+
+        <button class="btn">
+        <a href="{{ route('loaixe.list_loaixe') }}" class="ul-item text-decoration-none">Management LoaiXe</a>
+        </button>
+    </div>
+</div>
+
+                
+               
+              
+              
+                
+               
             </ul>
         </div>
     </section>
